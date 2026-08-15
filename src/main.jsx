4a9261af8001e0f35f08b1bf7ce8184e6c2e389e
nasty120hz-ux <nasty.120hz@gmail.com>
2026-08-15 20:41:12 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/gardelapeche">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
